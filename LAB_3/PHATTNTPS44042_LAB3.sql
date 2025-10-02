9ce1fa39a39fd208e0b5db28d603fe46f6d6074a
shannelly2810 <shannelly2810@gmail.com>
2025-10-02 14:42:06 +0700

    Nop bai

---
+++ LAB_3/PHATTNTPS44042_LAB3.sql
@@ -0,0 +1,127 @@
+﻿USE QLDA;
+------------------------------------------------------1.1
+-- Dùng CAST
+SELECT MA_NVIEN, MADA, STT, 
+       CAST(THOIGIAN AS varchar(10)) AS ThoiGian_text
+FROM PHANCONG;
+
+-- Dùng CONVERT
+SELECT MA_NVIEN, MADA, STT, 
+       CONVERT(varchar(10), THOIGIAN) AS ThoiGian_text
+FROM PHANCONG;
+------------------------------------------------------1.2
+-- Dùng CAST (decimal 10,2)
+SELECT d.TENDEAN,
+       CAST(SUM(p.THOIGIAN) AS decimal(10,2)) AS TongGio
+FROM DEAN d
+JOIN PHANCONG p ON d.MADA = p.MADA
+GROUP BY d.TENDEAN;
+
+-- Dùng CONVERT (decimal 10,2)
+SELECT d.TENDEAN,
+       CONVERT(decimal(10,2), SUM(p.THOIGIAN)) AS TongGio
+FROM DEAN d
+JOIN PHANCONG p ON d.MADA = p.MADA
+GROUP BY d.TENDEAN;
+
+-- Xuất dạng varchar
+SELECT d.TENDEAN,
+       CAST(CAST(SUM(p.THOIGIAN) AS decimal(10,2)) AS varchar(20)) AS TongGio_varchar
+FROM DEAN d
+JOIN PHANCONG p ON d.MADA = p.MADA
+GROUP BY d.TENDEAN;
+------------------------------------------------------1.3
+-- decimal(10,2)
+SELECT pb.TENPHG,
+       CAST(AVG(nv.LUONG) AS decimal(10,2)) AS LuongTB
+FROM PHONGBAN pb
+JOIN NHANVIEN nv ON pb.MAPHG = nv.PHG
+GROUP BY pb.TENPHG;
+
+-- varchar có phân cách hàng nghìn
+SELECT pb.TENPHG,
+       FORMAT(AVG(nv.LUONG), 'N2', 'vi-VN') AS LuongTB_varchar
+FROM PHONGBAN pb
+JOIN NHANVIEN nv ON pb.MAPHG = nv.PHG
+GROUP BY pb.TENPHG;
+------------------------------------------------------2.1
+SELECT d.TENDEAN,
+       CEILING(SUM(p.THOIGIAN)) AS Gio_Ceiling,
+       FLOOR(SUM(p.THOIGIAN))   AS Gio_Floor,
+       ROUND(SUM(p.THOIGIAN),2) AS Gio_Rounded
+FROM DEAN d
+JOIN PHANCONG p ON d.MADA = p.MADA
+GROUP BY d.TENDEAN;
+------------------------------------------------------2.2
+SELECT nv.HONV, nv.TENLOT, nv.TENNV, nv.LUONG
+FROM NHANVIEN nv
+JOIN PHONGBAN pb ON nv.PHG = pb.MAPHG
+WHERE nv.LUONG > (
+    SELECT ROUND(AVG(LUONG),2)
+    FROM NHANVIEN nv2
+    JOIN PHONGBAN pb2 ON nv2.PHG = pb2.MAPHG
+    WHERE pb2.TENPHG = N'Nghiên Cứu'
+);
+------------------------------------------------------3.1
+SELECT 
+  UPPER(nv.HONV) AS HoNV,
+  LOWER(nv.TENLOT) AS TenLot,
+  CASE
+    WHEN LEN(nv.TENNV) >= 2
+    THEN LOWER(LEFT(nv.TENNV,1)) 
+         + UPPER(SUBSTRING(nv.TENNV,2,1)) 
+         + LOWER(SUBSTRING(nv.TENNV,3,100))
+    ELSE UPPER(nv.TENNV)
+  END AS TenNV_custom,
+  -- Lấy phần tên đường trong địa chỉ (sau số nhà, trước dấu ',')
+  LTRIM(RTRIM(
+     SUBSTRING(nv.DCHI, CHARINDEX(' ', nv.DCHI)+1, 
+       CHARINDEX(',', nv.DCHI+',')-CHARINDEX(' ', nv.DCHI))
+  )) AS TenDuong
+FROM NHANVIEN nv
+WHERE nv.MANV IN (
+  SELECT MA_NVIEN 
+  FROM THANNHAN 
+  GROUP BY MA_NVIEN
+  HAVING COUNT(*) > 2
+);
+------------------------------------------------------3.2
+WITH CountNV AS (
+    SELECT PHG, COUNT(*) AS SoNV
+    FROM NHANVIEN
+    GROUP BY PHG
+),
+MaxPhong AS (
+    SELECT TOP 1 PHG FROM CountNV ORDER BY SoNV DESC
+)
+SELECT pb.TENPHG,
+       ISNULL(nv.HONV + ' ' + nv.TENLOT + ' ' + nv.TENNV, '(Chưa có)') AS TruongPhong,
+       'Fpoly' AS TruongPhong_Fpoly
+FROM PHONGBAN pb
+JOIN MaxPhong mp ON pb.MAPHG = mp.PHG
+LEFT JOIN NHANVIEN nv ON pb.TRPHG = nv.MANV;
+
+------------------------------------------------------4.1
+SELECT HONV, TENLOT, TENNV, NGSINH
+FROM NHANVIEN
+WHERE YEAR(NGSINH) BETWEEN 1960 AND 1965;
+------------------------------------------------------4.2
+SELECT HONV, TENLOT, TENNV, NGSINH,
+       DATEDIFF(year, NGSINH, GETDATE())
+         - CASE WHEN DATEADD(year, DATEDIFF(year, NGSINH, GETDATE()), NGSINH) > GETDATE() 
+                THEN 1 ELSE 0 END AS Tuoi
+FROM NHANVIEN;
+------------------------------------------------------4.3
+SELECT HONV, TENLOT, TENNV, NGSINH,
+       FORMAT(NGSINH, 'dddd', 'vi-VN') AS ThuSinh
+FROM NHANVIEN;
+
+------------------------------------------------------4.4
+SELECT pb.TENPHG,
+       COUNT(nv.MANV) AS SoNhanVien,
+       ISNULL(tp.HONV + ' ' + tp.TENLOT + ' ' + tp.TENNV, '(Chưa có)') AS TruongPhong,
+       CONVERT(varchar(8), pb.NG_NHANCHUC, 5) AS NgayNhanChuc_ddmmyy
+FROM PHONGBAN pb
+LEFT JOIN NHANVIEN nv ON pb.MAPHG = nv.PHG
+LEFT JOIN NHANVIEN tp ON pb.TRPHG = tp.MANV
+GROUP BY pb.TENPHG, pb.NG_NHANCHUC, tp.HONV, tp.TENLOT, tp.TENNV;
---
+++ LAB_4/PHATTNTPS44042_LAB4.sql
@@ -0,0 +1,83 @@
+﻿USE QLDA;
+------------------------------------------------------1.1
+SELECT nv.TENNV,
+       CASE 
+         WHEN nv.LUONG < (SELECT AVG(LUONG) 
+                          FROM NHANVIEN 
+                          WHERE PHG = nv.PHG)
+         THEN N'TangLuong'
+         ELSE N'KhongTangLuong'
+       END AS TrangThai
+FROM NHANVIEN nv;
+------------------------------------------------------1.2
+SELECT nv.TENNV,
+       CASE 
+         WHEN nv.LUONG < (SELECT AVG(LUONG) 
+                          FROM NHANVIEN 
+                          WHERE PHG = nv.PHG)
+         THEN N'NhanVien'
+         ELSE N'TruongPhong'
+       END AS LoaiNV
+FROM NHANVIEN nv;
+------------------------------------------------------1.3
+SELECT CASE 
+         WHEN PHAI LIKE N'Nam' THEN N'Ông ' + TENNV
+         WHEN PHAI LIKE N'N?'  THEN N'Bà ' + TENNV
+         ELSE TENNV
+       END AS HoTenTheoPhai
+FROM NHANVIEN;
+------------------------------------------------------1.4
+SELECT TENNV, LUONG,
+       CASE 
+         WHEN LUONG > 0     AND LUONG < 25000 THEN LUONG * 0.10
+         WHEN LUONG >=25000 AND LUONG < 30000 THEN LUONG * 0.12
+         WHEN LUONG >=30000 AND LUONG < 40000 THEN LUONG * 0.15
+         WHEN LUONG >=40000 AND LUONG < 50000 THEN LUONG * 0.20
+         WHEN LUONG >=50000 THEN LUONG * 0.25
+         ELSE 0
+       END AS Thue
+FROM NHANVIEN;
+------------------------------------------------------2.1
+DECLARE @i int = 0;
+
+WHILE @i <= (SELECT MAX(CAST(MANV AS int)) FROM NHANVIEN)
+BEGIN
+    SET @i = @i + 1;
+
+    IF @i % 2 = 0
+       SELECT HONV, TENLOT, TENNV
+       FROM NHANVIEN
+       WHERE MANV = RIGHT('000' + CAST(@i AS varchar(3)),3); -- MANV dạng chuỗi
+END
+------------------------------------------------------2.2
+DECLARE @i int = 0;
+
+WHILE @i <= (SELECT MAX(CAST(MANV AS int)) FROM NHANVIEN)
+BEGIN
+    SET @i = @i + 1;
+
+    IF @i % 2 = 0 AND @i <> 4
+       SELECT HONV, TENLOT, TENNV
+       FROM NHANVIEN
+       WHERE MANV = RIGHT('000' + CAST(@i AS varchar(3)),3);
+END
+------------------------------------------------------3.1
+BEGIN TRY
+    INSERT INTO PHONGBAN(TENPHG, MAPHG, TRPHG, NG_NHANCHUC)
+    VALUES (N'TestPhong', 99, NULL, GETDATE());
+
+    PRINT N'Thêm dữ liệu thành công';
+END TRY
+BEGIN CATCH
+    PRINT N'Thêm dữ liệu thất bại';
+END CATCH;
+------------------------------------------------------3.2
+DECLARE @chia int = 10, @mau int = 0;
+
+BEGIN TRY
+    DECLARE @kq int;
+    SET @kq = @chia / @mau;  -- l?i chia 0
+END TRY
+BEGIN CATCH
+    RAISERROR(N'Lỗi: phép chia cho 0 không hợp lệ',16,1);
+END CATCH;
